8de23f73f1f9eca35f181e091fdaa1f7bb615768
Aleksandr <sashabogush@gmail.com>
2020-11-15 19:32:13 +0200

    up-to-date

--- src/main/java/hotel/services/RoomService.java
+++ src/main/java/hotel/services/RoomService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
