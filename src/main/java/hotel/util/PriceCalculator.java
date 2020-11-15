ba66aec7bee3b42495c4e2a3a6cf712758a9482b
Aleksandr <sashabogush@gmail.com>
2020-11-15 19:31:46 +0200

    up-to-date

--- src/main/java/hotel/util/PriceCalculator.java
+++ src/main/java/hotel/util/PriceCalculator.java
@@ -5,6 +5,12 @@ import hotel.entities.Room;
 
 import java.util.List;
 
+/**
+ * @author Bogush Aleksandr
+ * @version 1.0
+ * @since 15-11-2020
+ */
+
 public class PriceCalculator {
     public static double getPrice(Room room, List<Feature> features) {
 
