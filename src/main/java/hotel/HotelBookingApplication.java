f416996eb3347392546623363e6ea0182a21e022
Aleksandr <sashabogush@gmail.com>
2020-11-15 19:31:23 +0200

    up-to-date

--- src/main/java/hotel/HotelBookingApplication.java
+++ src/main/java/hotel/HotelBookingApplication.java
@@ -3,9 +3,14 @@ package hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Bogush Aleksandr
+ * @version 1.0
+ * @since 15-11-2020
+ */
+
 @SpringBootApplication
 public class HotelBookingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HotelBookingApplication.class, args);
     }
